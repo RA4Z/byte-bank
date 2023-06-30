cd6449fc8382fd1488ebbd6be9ba664ebb5df203
RA4Z <rraz639@gmail.com>
2023-06-30 10:54:50 -0300

    Fazendo a busca de palavras no Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 TestaArrayInt();
+TestaBuscarPalavra();
 
 void TestaArrayInt()
 {
@@ -22,4 +23,26 @@ void ListarIdades(int[] idades)
         somaIdades += idades[i];
     }
     Console.WriteLine($"\nSoma de todas as idades no Array => {somaIdades}");
+}
+
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+    for(int i = 0; i < arrayDePalavras.Length ; i++)
+    {
+        Console.WriteLine($"Digite a palavra do índice {i+1} => ");
+        arrayDePalavras[i] = Console.ReadLine()!;
+    }
+    Console.WriteLine("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach(string palavra in arrayDePalavras)
+    {
+        if(palavra.Equals(busca))
+        {
+            Console.WriteLine($"A palavra {palavra} foi encontrada no Array!");
+            return;
+        }
+    }
+    Console.WriteLine($"A palavra {busca} não foi encontrada no Array!");
 }
